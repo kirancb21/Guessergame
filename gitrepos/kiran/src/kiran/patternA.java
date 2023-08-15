6ee51be17d43ce42f7e6e17324a35bca009b37c8
kirancb21 <139037919+kirancb21@users.noreply.github.com>
2023-08-15 19:58:20 +0530

    Add files via upload

---
+++ gitrepos/kiran/src/kiran/patternA.java
@@ -0,0 +1,24 @@
+package kiran;
+
+public class patternA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=0;i<=n;i++) {
+			for(int j=0;j<=n;j++) {
+				if((i==0 && j>=n/4 && j>=(3*n)/4) ||
+						(j==0 && i>=n/49 )||
+						(i==(n-1)/2)||
+						(j==n-1 && i>n/4 && i==(n-1)/2)) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ gitrepos/kiran/src/kiran/patternO.java
@@ -0,0 +1,21 @@
+package kiran;
+
+public class patternO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10;
+		for(int i=0;i<=n;i++) {
+			for(int j=0;j<=n;j++) {
+				if((i==(n)/4 && j>n/4 && j<(3*n)/4)|| (j==n/4 && i>n/4 && i<3*n/4) || (i==3*n/4 && j>n/4 && j<3*n/4) || j==3*n/4 && i>n/4 && i<(3*n)/4){
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+
+			}
+			System.out.println();
+		}
+	}
+
+}
